6c59180cebc228cbf5fe472991b28957689c8fb2
noumiso <blainequigley44@gmail.com>
2025-02-02 01:41:20 +0900

    reads character count in book Frankenstein

---
+++ main.py
@@ -0,0 +1,32 @@
+import string
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    split = split_dict(chars_dict)
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def split_dict(chars_dict):
+    alphabet = list(string.ascii_lowercase) 
+    for item, count in chars_dict.items():
+        if item in alphabet:
+            print(f"The '{item}' character was found {count} times")
+main()
